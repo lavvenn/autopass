75b30199d8194b1cfa443b96887f719e3b8a7961
Konstantin.zmievskij <Konstantin.zmievskij@yandex.ru>
2025-12-08 17:14:48 +0300

    feat(pass):
    - create pass app
    - create Pass model with photo, user, status field

---
+++ autopass/pass/models.py
@@ -0,0 +1,29 @@
+__all__ = ()
+
+import django.contrib.auth.models
+import django.db.models
+from django.utils.translation import gettext_lazy as _
+
+
+class Pass(django.db.models):
+
+    class RatingChoices(django.db.models.TextChoices):
+        NotVerify = "NotUnVerify", _('Не проверен')
+        Verify = "Verify", _('Проверен')
+        Printed = "Printed", _('Нейтрально')
+
+
+    photo = django.db.models.ImageField(
+        upload_to="pass_photo/%Y/%m/%d/",
+        verbose_name=_("Фото на пропуск"),
+    )
+    user = django.db.models.OneToOneField(
+        django.contrib.auth.models.User,
+        on_delete=django.db.models.CASCADE,
+    )
+    status = django.db.models.CharField(
+        'статус',
+        choices=,
+        default='received',
+    )
+
