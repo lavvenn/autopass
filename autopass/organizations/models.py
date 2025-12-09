ef8cf8a00cd6205e2abb6bcb681f4fffb77ce761
Gobziii <gobziii@yandex.ru>
2025-12-09 19:06:13 +0300

    Update madels? added some fields

--- autopass/organizations/models.py
+++ autopass/organizations/models.py
@@ -19,15 +19,31 @@ class Institution(django.db.models.Model):
         "краткое название",
         max_length=50,
     )
+    information = django.db.models.CharField(
+        "описание",
+        max_length=5000,
+    )
     logo = django.db.models.ImageField(
         "логотип учебного заведения",
         upload_to="institutions/",
         blank=True,
         null=True,
     )
+    admin = django.db.models.ForeignKey(
+        "CustomUser",
+        verbose_name="администратор организации",
+        on_delete=django.db.models.CASCADE,
+        related_name="created_institutions",
+    )
 
     def __str__(self):
         return self.short_name
+    
+    def save(self, *args, **kwargs):
+        if not self.pk and not self.creator:
+            # take user from request
+            pass
+        super().save(*args, **kwargs)
 
 
 class Group(django.db.models.Model):
@@ -54,6 +70,21 @@ class Group(django.db.models.Model):
     year = django.db.models.DateField(
         "год поступления",
     )
+    curator = django.db.models.ForeignKey(
+        "CustomUser",
+        verbose_name="куратор",
+        on_delete=django.db.models.CASCADE,
+        related_name="curated_groups",
+        limit_choices_to={
+            "role": "curator",
+        },
+    )
+    institution = django.db.models.ForeignKey(
+        "Institution",
+        verbose_name="учебное заведение",
+        on_delete=django.db.models.CASCADE,
+        related_name="groups",
+    )
 
     def __str__(self):
-        return f"{self.name} ({self.course} курс)"
+        return f"{self.name} ({self.institution.short_name})"
