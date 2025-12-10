651bbe18b821def4228054108445f5566834b08a
Konstantin.zmievskij <Konstantin.zmievskij@yandex.ru>
2025-12-10 22:25:39 +0300

    feat(passes): - add pass creation after form

--- autopass/passes/views.py
+++ autopass/passes/views.py
@@ -1,10 +1,11 @@
 __all__ = ()
 
+import card_maker.card_maker
 import django.contrib
 import django.urls
 import django.views.generic
-
 import passes.forms
+import passes.models
 
 
 class PassFormView(django.views.generic.FormView):
@@ -15,9 +16,26 @@ class PassFormView(django.views.generic.FormView):
 
     def form_valid(self, form):
         user = django.contrib.auth.get_user_model()
-
         user_pass = form.save(commit=False)
         user_pass.user = user.objects.get(pk=self.request.user.id)
         user_pass = form.save()
+
+        name = f"{self.request.user.first_name} {self.request.user.last_name}"
+
+        im = card_maker.card_maker.ImageEditor(
+            template_path="template.png",
+            output_path="output",
+            circle_size=(250, 250),
+            photo_position=(50, 100),
+            text_position=(350, 400),
+        )
+
+        im.create_final_image(
+            image_path=passes.models.Pass.objects.get(
+                user=user.objects.get(pk=self.request.user.id),
+            ).photo.path,
+            text=name,
+            final_name=f"{name}.png",
+        )
         django.contrib.messages.success(self.request, "Пропуск сохранён")
         return super().form_valid(form)
